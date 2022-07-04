fcb2d8ef7d9cc8931fc076f2d24da488713cadf1
Aaron Szura <aaron.szura@gmail.com>
2022-07-04 11:53:04 +0100

    fix styling for new cards created

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-class book {
-    constructor (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
-const warAndPeace = new book("War and Peace", "Leo Tolstoy", 1000, false);
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1000, false);
+const catch22 = new Book("Catch-22", "Joseph Heller", "Leo Tolstoy", 523, true);
 
 function createCard() {
 
@@ -26,10 +27,11 @@ function createCard() {
     this.content.classList.add("card-content");
     this.card.appendChild(this.content);
 
-    this.readBtn =  document.createElement("input");
+    this.readBtn = document.createElement("input");
     this.readBtn.classList.add("read")
+    this.readBtn.setAttribute("type", "radio");
     this.card.appendChild(this.readBtn);
-    
+
     this.title = document.createElement("h1");
     this.content.appendChild(this.title);
 
@@ -45,18 +47,24 @@ function createCard() {
 
 const dom = {
 
-    library : document.querySelector(".library"),
+    library: document.querySelector(".library"),
 
 }
 
 const library = document.querySelector(".library");
 
 
-const books = [warAndPeace];
+const books = [warAndPeace, catch22];
 
-for (let book of books) {
-    let card = new createCard();
-    card.title.innerText = book.title;
-    card.author.innerText = book.author;
-    dom.library.appendChild(card.card);
+function refreshLibrary() {
+
+    for (let book of books) {
+        let card = new createCard();
+        card.title.innerText = book.title;
+        card.author.innerText = book.author;
+        card.card.setAttribute("data", "0");
+        dom.library.appendChild(card.card);
+    }
 }
+
+refreshLibrary();
